511e15c15b30ea335005f21b58eb246885a79d54
Fatin110 <ishmam110@gmail.com>
2024-10-05 17:16:00 +0600

    login and registration logic

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +17,21 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd('kitaba');
+        //validate
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        //create the user
+        $user = User::create($attributes);
+
+        //log in
+        Auth::login($user);
+
+        //redirect
+        return redirect('/jobs');
     }
 }
